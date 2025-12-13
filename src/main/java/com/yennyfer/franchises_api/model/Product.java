d9e400d7007c4c0db37059c732280f342001e8c7
Yennyferjp <yennyfer.jaravapz@gmail.com>
2025-12-13 12:57:23 -0500

    feat: agregar entidad Product - Se crea la entidad Product con los atributos id, name y stock - Se establece la relación con Branch mediante el atributo branchId - Se utiliza la anotación @Table para mapear la clase a la tabla "product"

---
+++ src/main/java/com/yennyfer/franchises_api/model/Product.java
@@ -0,0 +1,29 @@
+package com.yennyfer.franchises_api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("product")
+public class Product {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private Integer stock;
+
+    @Column("branch_id")
+    private Long branchId;
+}
--- src/main/java/com/yennyfer/franchises_api/model/model.java
+++
@@ -1,4 +0,0 @@
-package com.yennyfer.franchises_api.model;
-
-public class model {
-}
