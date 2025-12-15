6f2625f7a000a4365c8fb469febcb7ee77b8f363
Yennyferjp <yennyfer.jaravapz@gmail.com>
2025-12-14 22:44:58 -0500

    feat(model): modificar entidad Branch  - Se edita el campo name  - Se agrega el campo address

--- src/main/java/com/yennyfer/franchises_api/model/Branch.java
+++ src/main/java/com/yennyfer/franchises_api/model/Branch.java
@@ -1,28 +1,31 @@
-package com.yennyfer.franchises_api.model;
+    package com.yennyfer.franchises_api.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
+    import lombok.AllArgsConstructor;
+    import lombok.Builder;
+    import lombok.Getter;
+    import lombok.NoArgsConstructor;
+    import lombok.Setter;
+    import org.springframework.data.annotation.Id;
+    import org.springframework.data.relational.core.mapping.Column;
+    import org.springframework.data.relational.core.mapping.Table;
 
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Table("branch")
-public class Branch {
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Table("branch")
+    public class Branch {
 
-    @Id
-    private Long id;
+        @Id
+        private Long id;
 
-    @Column("nombre")
-    private String nombre;
+        @Column("name")
+        private String name;
 
-    @Column("franchise_id")
-    private Long franchiseId;
-}
+        @Column("address")
+        private String address;
+
+        @Column("franchise_id")
+        private Long franchiseId;
+    }
