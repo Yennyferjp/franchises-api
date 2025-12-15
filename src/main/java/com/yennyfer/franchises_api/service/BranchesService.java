c6f1e5cc0dd950291be3f3af8d293dd58bb7e316
Yennyferjp <yennyfer.jaravapz@gmail.com>
2025-12-14 22:41:44 -0500

    feat: agregar actualización y eliminación de sucursales

--- src/main/java/com/yennyfer/franchises_api/service/BranchesService.java
+++ src/main/java/com/yennyfer/franchises_api/service/BranchesService.java
@@ -1,11 +1,15 @@
 package com.yennyfer.franchises_api.service;
 
+import com.yennyfer.franchises_api.dto.UpdateBranchRequest;
 import com.yennyfer.franchises_api.model.Branch;
 import com.yennyfer.franchises_api.model.BranchAggregate;
 import com.yennyfer.franchises_api.repository.BranchRepository;
 import com.yennyfer.franchises_api.repository.ProductsRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -16,8 +20,18 @@ public class BranchesService {
     private final BranchRepository branchRepository;
     private final ProductsRepository productsRepository;
 
-    public Mono<Branch> createBranch(Branch branch){
-        return branchRepository.save(branch);
+    public Mono<Branch> createBranch(Branch branch) {
+        return branchRepository.findByName(
+                        branch.getName()
+                ).collectList()
+                .flatMap(existingBranches -> {
+                    ;
+                    if (!existingBranches.isEmpty()) {
+                        return Mono.error(new IllegalArgumentException("La sucursal con el nombre proporcionado ya existe."));
+                    }
+                    return branchRepository.save(branch);
+                });
+
     }
 
     public Flux<Branch> getAllBranches(){
@@ -32,5 +46,42 @@ public class BranchesService {
         return productsRepository.findByBranchId(branch.getId())
                 .collectList()
                 .map(products -> new BranchAggregate(branch, products));
+    }
+
+
+    public Mono<Branch> updateBranch(
+            Long branchId,
+            @Valid UpdateBranchRequest request) {
+
+        if (request == null) {
+            return Mono.error(
+                    new IllegalArgumentException("El cuerpo de la petición es obligatorio")
+            );
+        }
+        return branchRepository.findById(branchId)
+                .switchIfEmpty(Mono.error(
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Sucursal no encontrada")
+                ))
+                .flatMap(branch -> {
+                    branch.setName(request.name());
+                    branch.setAddress(request.address());
+                    branch.setFranchiseId(request.franchiseId());
+                    return branchRepository.save(branch);
+                });
+    }
+
+    public Mono<Void> deleteBranch(Long branchId) {
+        return branchRepository.existsById(branchId)
+                .flatMap(exists -> {
+                    if (!exists) {
+                        return Mono.error(
+                                new ResponseStatusException(
+                                        HttpStatus.NOT_FOUND,
+                                        "La sucursal no existe"
+                                )
+                        );
+                    }
+                    return branchRepository.deleteById(branchId);
+                });
     }
 }
